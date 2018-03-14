af0c23272d6b2e2e0372372be9d5f853825fef7f
Nicolas <nicolasbs@al.insper.edu.br>
2018-03-14 19:21:19 -0300

    equaçãodogrupo

--- Projetos/C-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/C-LogicaCombinacional/src/TopLevel.vhd
@@ -19,19 +19,61 @@ use work.all;
 ----------------------------
 entity TopLevel is
 	port(
-		a : in std_logic;
-		b : in std_logic;
-		c : in std_logic;
-
-		q : out std_logic);
+		SW0      : in  std_logic_vector(9 downto 0);
+		SW1		: in std_logic_vector(9 downto 0);
+		SW2		: in std_logic_vector(9 downto 0);
+		LEDR     : out std_logic_vector(9 downto 0)
+	);
+		
 end entity;
 
 ----------------------------
 -- Implementacao do bloco -- 
 ----------------------------
 architecture rtl of TopLevel is
-begin
- 
-	q <= not a or (b and c);
+--sub-module(half adder) is declared as a component before the keyword "begin".
+	component And16
+		port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			b:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+	end component;
 
+	component Or16
+		port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			b:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+	end component;
+	
+	component Not16
+		port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+	end component;
+--------------
+-- signals
+--------------
+signal s1,c1,c2 : std_logic:='0';
+---------------
+-- implementacao
+---------------
+begin
+	NOT1 : Not16 portmap (
+			 a => SW0,
+			 q => s1
+			 );
+
+	AND1 : And16 portmap (
+          a => SW1,
+          b => SW2,
+          q => c1
+        );
+		
+	OR1 : Or16 portmap (
+          a => s1,
+          b => c1,
+          q => LEDR(0)
+        );
+ q <= LEDR(0);
 end rtl;
