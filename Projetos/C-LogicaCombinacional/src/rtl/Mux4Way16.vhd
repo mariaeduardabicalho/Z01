d62a764af529f58f88eab0174532172eef7fa023
pedrooa <pedroazambuja14@gmail.com>
2018-03-14 16:32:27 -0300

    mux16,mux4way16,mux8way16

--- Projetos/C-LogicaCombinacional/src/rtl/Mux4Way16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/Mux4Way16.vhd
@@ -10,3 +10,19 @@ entity Mux4Way16 is
 			sel: in  STD_LOGIC_VECTOR(1 downto 0);
 			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
+architecture pep of Mux4Way16 is
+begin
+process (a,b,c,d,sel) is
+begin
+  if (sel(0) ='0' and sel(1) = '0') then
+      q <= a;
+  elsif (sel(0) ='1' and sel(1) = '0') then
+      q <= b;
+  elsif (sel(0) ='0' and sel(1) = '1') then
+      q <= c;
+  else
+      q <= d;
+  end if;
+  
+end process;
+end pep;
--- Projetos/C-LogicaCombinacional/src/rtl/Mux8Way16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/Mux8Way16.vhd
@@ -14,3 +14,18 @@ entity Mux8Way16 is
 			sel: in  STD_LOGIC_VECTOR(2 downto 0);
 			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
+
+architecture pep of Mux8Way16 is
+begin
+process (a,b,c,d,e,f,g,h,sel) is
+begin
+q <= a when sel = "000";
+q <= b when sel = "001";
+q <= c when sel = "010";
+q <= d when sel = "011";
+q <= e when sel = "100";
+q <= f when sel = "101";
+q <= g when sel = "110";
+q <= h when sel = "111";
+end process;
+end pep;
