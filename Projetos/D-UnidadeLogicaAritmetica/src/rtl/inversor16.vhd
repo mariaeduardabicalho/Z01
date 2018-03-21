5530d1623888b47febac592e5e40b51afd20d09a
elijose55 <elijose55@hotmail.com>
2018-03-21 11:41:06 -0300

    inversor16 e imagem de onda

--- Projetos/D-UnidadeLogicaAritmetica/src/rtl/inversor16.vhd
+++ Projetos/D-UnidadeLogicaAritmetica/src/rtl/inversor16.vhd
@@ -4,16 +4,27 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
+use ieee.numeric_std.all;
+use work.all;
 
 entity inversor16 is
    port(z   : in STD_LOGIC;
-	     a   : in STD_LOGIC_VECTOR(15 downto 0);
+	    a   : in STD_LOGIC_VECTOR(15 downto 0);
         y   : out STD_LOGIC_VECTOR(15 downto 0)
    );
 end entity;
 
 architecture rtl of inversor16 is
+	component Not16
+		port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+	end component;
 
 begin
+  y <= a when z = '0'  -- quando o controle z = 0, a saida sera o proprio vetor
+
+  else not a;  -- caso contrario, a saida sera o vetor de entrada invertido
+
 
 end architecture;
