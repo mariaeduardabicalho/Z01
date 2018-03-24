7dd00e096cf51421cb3850f9af8ae4089e7f4ea6
Thomas Pilnik <thomaspo@al.insper.edu.br>
2018-03-24 14:02:12 -0300

    flip flop testado e funcionando

--- Projetos/E-LogicaSequencial/src/rtl/FlipFlopD.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/FlipFlopD.vhd
@@ -1,7 +1,3 @@
--- Elementos de Sistemas
--- by Luciano Soares
--- FlipFlopD.vhd
-
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -18,5 +14,15 @@ end entity;
 architecture arch of FlipFlopD is
 begin
 
+	process (clock,preset,clear) begin
+		if (clear = '1') then     -- zera a saída do flip flop
+			q<='0';
+		elsif (preset='1') then   -- coloca um na saída
+			q<='1';
+		elsif (rising_edge(CLOCK)) then    -- verifica se o processo foi de borda de subida 
+			q<=d;						   -- se for, atribui novo sinal, se não for, mantem sinal anterior
+	end if;
+
+end process;
 
 end architecture;
