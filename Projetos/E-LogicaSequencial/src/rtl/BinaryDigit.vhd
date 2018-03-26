6b01fc5663e9daa8678b820fec69305490b1319e
Thomas Pilnik <thomaspo@al.insper.edu.br>
2018-03-25 23:03:30 -0300

    register8 testado e funcionando

--- Projetos/E-LogicaSequencial/src/rtl/BinaryDigit.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/BinaryDigit.vhd
@@ -13,3 +13,27 @@ entity BinaryDigit is
 		output: out STD_LOGIC
 	);
 end entity;
+
+architecture arch of BinaryDigit is
+	component Mux2Way
+		port(
+		a,b,sel: in STD_LOGIC;
+		q: out STD_LOGIC
+	);
+	end component;
+	component FlipFlopD
+		port (
+		clock, d, clear, preset: in STD_LOGIC;
+		q:out STD_LOGIC
+	);
+	end	component;
+
+signal s1, s2: std_logic;
+
+begin 
+
+	c1: Mux2Way port map(s2, input, load, s1);
+	c2: FlipFlopD port map(clock, s1, '0','0',s2);
+	output <= s2;
+
+end	architecture;
