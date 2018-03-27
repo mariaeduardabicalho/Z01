f9a5b2a195b816e457406118a092ae2789b9333c
pedrooa <pedroazambuja14@gmail.com>
2018-03-27 18:55:53 -0300

    Modulos Register8 e Register16 implementados e testados

--- Projetos/E-LogicaSequencial/src/rtl/Register8.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Register8.vhd
@@ -13,3 +13,31 @@ entity Register8 is
 		output: out STD_LOGIC_VECTOR(7 downto 0)
 	);
 end entity;
+
+architecture arch of Register8 is
+	component BinaryDigit
+		port(
+			clock:   in STD_LOGIC;
+			input:   in STD_LOGIC;
+			load:    in STD_LOGIC;
+			output: out STD_LOGIC
+		);
+	end component;
+
+begin 
+	m0: BinaryDigit port map(clock, input(0),load,output(0));
+	m1: BinaryDigit port map(clock, input(1),load,output(1));
+	m2: BinaryDigit port map(clock, input(2),load,output(2));
+	m3: BinaryDigit port map(clock, input(3),load,output(3));
+	m4: BinaryDigit port map(clock, input(4),load,output(4));
+	m5: BinaryDigit port map(clock, input(5),load,output(5));
+	m6: BinaryDigit port map(clock, input(6),load,output(6));
+	m7: BinaryDigit port map(clock, input(7),load,output(7));
+
+end architecture;
+
+
+
+
+
+
