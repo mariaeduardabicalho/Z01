dc30056a54b2d9608fc086b22e8294e00f983704
pedrooa <pedroazambuja14@gmail.com>
2018-03-28 16:10:26 -0300

    Modulos Register 32 e 64 feitos pelo Guilherme Aliperti e testados pelo pedro Azambuja

--- Projetos/E-LogicaSequencial/src/rtl/Register32.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Register32.vhd
@@ -2,9 +2,9 @@
 -- by Luciano Soares
 -- Register32.vhd
 
-Library ieee; 
+Library ieee;
 use ieee.std_logic_1164.all;
-  
+
 entity Register32 is
 	port(
 		clock:   in STD_LOGIC;
@@ -13,3 +13,18 @@ entity Register32 is
 		output: out STD_LOGIC_VECTOR(31 downto 0)
 	);
 end entity;
+
+architecture arch of Register32 is
+	component Register16
+		port(
+			clock:   in STD_LOGIC;
+			input:   in STD_LOGIC_VECTOR(15 downto 0);
+			load:    in STD_LOGIC;
+			output: out STD_LOGIC_VECTOR(15 downto 0)
+		);
+	end component;
+
+begin
+	r1 : Register16 port map(clock, input(15 downto 0), load, output(15 downto 0));
+	r2 : Register16 port map(clock, input(31 downto 16), load, output(31 downto 16));
+end architecture;
