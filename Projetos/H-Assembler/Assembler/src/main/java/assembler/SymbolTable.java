69aa5f1f07aaf48028e80ce5d7c4bbe8672548bf
Thomas Pilnik <pilnikthomas@gmail.com>
2018-05-06 17:22:53 -0300

    Adicionando SymbolTable

--- Projetos/H-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
+++ Projetos/H-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
@@ -5,16 +5,33 @@
 
 package assembler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Mantém uma tabela com a correspondência entre os rótulos simbólicos e endereços numéricos de memória.
  */
 public class SymbolTable {
+
+	Map<String,Integer> Dictionary = new HashMap<String,Integer>();
 
     /**
      * Cria a tabela de símbolos.
      */
     public SymbolTable() {
 
+    	for (int i=0;i<16;i++) {
+    		Dictionary.put("R"+Integer.toString(i), i);
+    	}
+
+    	Dictionary.put( "SP", new Integer(0));
+    	Dictionary.put( "LCL", new Integer(1));
+    	Dictionary.put( "ARG", new Integer(2));
+    	Dictionary.put( "THIS", new Integer(3));
+    	Dictionary.put( "THAT", new Integer(4));
+    	Dictionary.put( "SCREEN", new Integer(16384));
+    	Dictionary.put( "LED", new Integer(21184));
+    	Dictionary.put( "SW", new Integer(21185));
     }
 
     /**
@@ -23,6 +40,7 @@ public class SymbolTable {
      * @param  address símbolo a ser armazenado na tabela de símbolos.
      */
     public void addEntry(String symbol, int address) {
+    	Dictionary.put(symbol, address);
 
     }
 
@@ -32,7 +50,7 @@ public class SymbolTable {
      * @return Verdadeiro se símbolo está na tabela de símbolos, Falso se não está na tabela de símbolos.
      */
     public Boolean contains(String symbol) {
-    	return null;
+    	return Dictionary.containsKey(symbol);
     }
 
     /**
@@ -41,7 +59,7 @@ public class SymbolTable {
      * @return valor numérico associado ao símbolo procurado.
      */
     public Integer getAddress(String symbol) {
-    	return null;
+    	return Dictionary.get(symbol);
     }
 
 }
