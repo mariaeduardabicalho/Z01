43b19a9b6b1963975961eb20e371211a99ea2375
elijose55 <elijose55@hotmail.com>
2018-06-20 01:43:16 -0300

    fix

--- Projetos/H-Assembler/Assembler/src/main/java/assembler/Code.java
+++ Projetos/H-Assembler/Assembler/src/main/java/assembler/Code.java
@@ -23,30 +23,54 @@ public class Code {
      */
     public static String dest(String[] mnemnonic) {
     	
-    	int length = mnemnonic.length - 1;
-    	StringBuilder myName = new StringBuilder("0000");
+    	// MOVW %A, %D
+    	// [MOVW, %A, %D]
+    	// JMP %D
+    	// [JMP, %D]
+    	String codig = "0000";
+        int size = mnemnonic.length;
+        StringBuilder codigo = new StringBuilder("0000"); //deixar em 0000 para quando o comando for null
+
+        
     	
-    	boolean bool = false; // Para os casos de incw, negw, etc. q tem apenas 2 de .length
-    	if (length == 1){
-    		bool = true;
+    	if (mnemnonic[(size-1)  ] == "%A"){
+    		codigo.setCharAt(0, '1');
     	}
-    	while (length >= 2 | bool == true) {
-    	if (mnemnonic[length].equals( "%A")){
-    		myName.setCharAt(0, '1');
-    	} else if (mnemnonic[length].equals( "%S")){
-    		myName.setCharAt(1, '1');
-    	} else if (mnemnonic[length].equals( "%D")){
-    		myName.setCharAt(2, '1');
-    	} else if (mnemnonic[length].equals( "(%A)")){
-    		myName.setCharAt(3, '1');
+    	if (mnemnonic[(size-1)] == "%D"){
+    		codigo.setCharAt(2, '1');
     	}
-    	bool = false;
-    	if (mnemnonic[0] != "movw"){
-    		length = 1;
+    	if (mnemnonic[(size-1)] == "%S"){
+    		codigo.setCharAt(1, '1');
     	}
-    	length -= 1;
+    	if (mnemnonic[(size-1)] == "(%A)"){
+    		codigo.setCharAt(3, '1');
     	}
-    	return myName.toString(); 
+    	
+
+        while (size >= 4 ) {
+        if (mnemnonic[size-1].equals( "%A")){
+            codigo.setCharAt(0, '1');
+        } else if (mnemnonic[size-1].equals( "%S")){
+            codigo.setCharAt(1, '1');
+        } else if (mnemnonic[size-1].equals( "%D")){
+            codigo.setCharAt(2, '1');
+        } else if (mnemnonic[size-1].equals( "(%A)")){
+            codigo.setCharAt(3, '1');
+        }
+        size -= 1;
+        }
+
+        if(mnemnonic.length < 2) {
+            return codig ;
+        }  
+        else if(mnemnonic[0].equals("jmp") || mnemnonic[0].equals("je") || mnemnonic[0].equals("jne") || mnemnonic[0].equals("jg") || mnemnonic[0].equals("jge") || mnemnonic[0].equals("jl") || mnemnonic[0].equals("jle") ) {
+            return codig;
+        }
+    	
+    	else{
+            return codigo.toString();
+        }
+	
     }
     
     /**
@@ -55,44 +79,47 @@ public class Code {
      * @return Opcode (String de 8 bits) com código em linguagem de máquina para a instrução.
      */
     public static String comp(String[] mnemnonic) {
-    	StringBuilder binCalc = new StringBuilder("10000000");
+    	StringBuilder codigoc = new StringBuilder("10000000");
  
     	
-    	int length = mnemnonic.length - 1;
+    	int size = mnemnonic.length - 1;
 
     	// A e B
-    	if (length == 1) {
+    	//para eles sozinhos, mov que eh so dois, e o resto q eh 3
+    	//colocando 01 de acordo com a tabela
+    	if (size == 1) {
     		if (mnemnonic[1].equals( "%A") | mnemnonic[1].equals( "%D")){
-        		binCalc.setCharAt(0, '0');
+        		codigoc.setCharAt(0, '0');
         	} else if(mnemnonic[1].equals( "%S")){
-        		binCalc.setCharAt(0, '0');
-        		binCalc.setCharAt(1, '1');
+        		codigoc.setCharAt(0, '0');
+        		codigoc.setCharAt(1, '1');
         	}
     	} else if(mnemnonic[0].equals( "movw")){
     		if ((mnemnonic[1].equals( "%A"))|(mnemnonic[1].equals( "%D"))){
-    			binCalc.setCharAt(0, '0');
+    			codigoc.setCharAt(0, '0');
     		} else if(mnemnonic[1].equals( "%S")){
-    			binCalc.setCharAt(0, '0');
-    			binCalc.setCharAt(1, '1');
+    			codigoc.setCharAt(0, '0');
+    			codigoc.setCharAt(1, '1');
     		}
-    	} else if(length >= 2) {
+
+    	} else if(size >= 2) {
     		if ((mnemnonic[1].equals( "%A") & mnemnonic[2].equals( "%D"))|(mnemnonic[2].equals( "%A") & mnemnonic[1].equals( "%D"))){
-    			binCalc.setCharAt(0, '0');
+    			codigoc.setCharAt(0, '0');
     		} else if ((mnemnonic[1].equals( "%A") & mnemnonic[2].equals( "%S"))|(mnemnonic[2].equals( "%A") & mnemnonic[1].equals( "%S"))){
-    			binCalc.setCharAt(0, '0');
-    			binCalc.setCharAt(1, '1');
+    			codigoc.setCharAt(0, '0');
+    			codigoc.setCharAt(1, '1');
     		} else if ((mnemnonic[1].equals( "(%A)") & mnemnonic[2].equals( "%S"))|(mnemnonic[2].equals("(%A)") & mnemnonic[1].equals( "%S"))){
-    			binCalc.setCharAt(1, '1');
+    			codigoc.setCharAt(1, '1');
     		}
     	} else if(mnemnonic.length == 1){
     		if (mnemnonic[0].equals( "nop")){
-    			binCalc.setCharAt(2, '1');
-    			binCalc.setCharAt(4, '1');
-    			binCalc.setCharAt(6, '1');
+    			codigoc.setCharAt(2, '1');
+    			codigoc.setCharAt(4, '1');
+    			codigoc.setCharAt(6, '1');
     		} else {
-    			binCalc.setCharAt(0, '0');
-        		binCalc.setCharAt(2, '1');
-        		binCalc.setCharAt(3, '1');
+    			codigoc.setCharAt(0, '0');
+        		codigoc.setCharAt(2, '1');
+        		codigoc.setCharAt(3, '1');
     		}
     		
     	}
@@ -101,121 +128,121 @@ public class Code {
     	
     	
     	if ((mnemnonic[0].equals( "incw"))){
-    		binCalc.setCharAt(3, '1');
+    		codigoc.setCharAt(3, '1');
     		if (mnemnonic[1].equals( "%A") | mnemnonic[1].equals( "(%A)")){
-    			binCalc.setCharAt(2, '1');
-    			binCalc.setCharAt(3, '1');
+    			codigoc.setCharAt(2, '1');
+    			codigoc.setCharAt(3, '1');
     		} else{
-    			binCalc.setCharAt(3, '1');
-    			binCalc.setCharAt(4, '1');
+    			codigoc.setCharAt(3, '1');
+    			codigoc.setCharAt(4, '1');
 
     		}
-			binCalc.setCharAt(5, '1');
-    		binCalc.setCharAt(6, '1');
-    		binCalc.setCharAt(7, '1');
+			codigoc.setCharAt(5, '1');
+    		codigoc.setCharAt(6, '1');
+    		codigoc.setCharAt(7, '1');
     	}
     	
-    	if ((mnemnonic[0].charAt(0) == 'j') & (length > 0) ){
+    	if ((mnemnonic[0].charAt(0) == 'j') & (size > 0) ){
     		if (mnemnonic[1].equals( "%S")){
-    			binCalc.setCharAt(1, '1');
+    			codigoc.setCharAt(1, '1');
     		}
-    		binCalc.setCharAt(4, '1');
-    		binCalc.setCharAt(5, '1');
+    		codigoc.setCharAt(4, '1');
+    		codigoc.setCharAt(5, '1');
     	}
     	
     	if ((mnemnonic[0].equals( "decw"))){
-    		binCalc.setCharAt(6, '1');
+    		codigoc.setCharAt(6, '1');
     		if (mnemnonic[1].equals( "%A") | mnemnonic[1].equals( "(%A)")){
-    			binCalc.setCharAt(2, '1');
-    			binCalc.setCharAt(3, '1');
+    			codigoc.setCharAt(2, '1');
+    			codigoc.setCharAt(3, '1');
     		} else{
-    			binCalc.setCharAt(4, '1');
-    			binCalc.setCharAt(5, '1');
+    			codigoc.setCharAt(4, '1');
+    			codigoc.setCharAt(5, '1');
     		}
     	}
     	
     	if ((mnemnonic[0].equals( "notw"))){
-    		binCalc.setCharAt(7, '1');
+    		codigoc.setCharAt(7, '1');
     		if (mnemnonic[1].equals( "%A") | mnemnonic[1].equals( "(%A)")){
-    			binCalc.setCharAt(2, '1');
-    			binCalc.setCharAt(3, '1');
+    			codigoc.setCharAt(2, '1');
+    			codigoc.setCharAt(3, '1');
     		} else{
-    			binCalc.setCharAt(4, '1');
-    			binCalc.setCharAt(5, '1');
+    			codigoc.setCharAt(4, '1');
+    			codigoc.setCharAt(5, '1');
     		}
     	}
     	
     	if ((mnemnonic[0].equals( "negw"))){
-    		binCalc.setCharAt(7, '1');
-    		binCalc.setCharAt(6, '1');
+    		codigoc.setCharAt(7, '1');
+    		codigoc.setCharAt(6, '1');
     		if (mnemnonic[1].equals( "%A") | mnemnonic[1].equals( "(%A)")){
-    			binCalc.setCharAt(2, '1');
-    			binCalc.setCharAt(3, '1');
+    			codigoc.setCharAt(2, '1');
+    			codigoc.setCharAt(3, '1');
     		} else{
-    			binCalc.setCharAt(4, '1');
-    			binCalc.setCharAt(5, '1');
+    			codigoc.setCharAt(4, '1');
+    			codigoc.setCharAt(5, '1');
     		}
     	}
     	
     	if ((mnemnonic[0].equals( "orw"))){
-    		binCalc.setCharAt(3, '1');
-    		binCalc.setCharAt(5, '1');
-    		binCalc.setCharAt(7, '1');
+    		codigoc.setCharAt(3, '1');
+    		codigoc.setCharAt(5, '1');
+    		codigoc.setCharAt(7, '1');
     	}
     	
     	if ((mnemnonic[0].equals( "nop"))){
-    		binCalc.setCharAt(2, '1');
-    		binCalc.setCharAt(4, '1');
-    		binCalc.setCharAt(6, '1');
+    		codigoc.setCharAt(2, '1');
+    		codigoc.setCharAt(4, '1');
+    		codigoc.setCharAt(6, '1');
     	}
     	
     	if ((mnemnonic[0].equals( "movw"))){
-    		binCalc.setCharAt(5, '0');
-    		binCalc.setCharAt(6, '0');
+    		codigoc.setCharAt(5, '0');
+    		codigoc.setCharAt(6, '0');
     		if (mnemnonic[1].equals( "%A") | mnemnonic[1].equals( "(%A)")){
-    			binCalc.setCharAt(2, '1');
-    			binCalc.setCharAt(3, '1');
+    			codigoc.setCharAt(2, '1');
+    			codigoc.setCharAt(3, '1');
     		} else {
-    			binCalc.setCharAt(4, '1');
-    			binCalc.setCharAt(5, '1');
+    			codigoc.setCharAt(4, '1');
+    			codigoc.setCharAt(5, '1');
     		}
     	}
     	
     	if ((mnemnonic[0].equals( "subw"))){
-    		binCalc.setCharAt(6, '1');
-			binCalc.setCharAt(7, '1'); //caso normal
+    		codigoc.setCharAt(6, '1');
+			codigoc.setCharAt(7, '1'); //caso normal
 			if (mnemnonic[1].equals( "%A")| mnemnonic[1].equals( "(%A)")){
-				binCalc.setCharAt(5, '1');
+				codigoc.setCharAt(5, '1');
 			} else{
-				binCalc.setCharAt(3, '1');
+				codigoc.setCharAt(3, '1');
 			}
 			if(mnemnonic[2].equals( "$1")) {
-				binCalc.setCharAt(2, '1');
-				binCalc.setCharAt(3, '1');
-				binCalc.setCharAt(5, '0');
-				binCalc.setCharAt(7, '0');
+				codigoc.setCharAt(2, '1');
+				codigoc.setCharAt(3, '1');
+				codigoc.setCharAt(5, '0');
+				codigoc.setCharAt(7, '0');
 			}
     	}
     	
     	if ((mnemnonic[0].equals( "rsubw"))){
-    		binCalc.setCharAt(6, '1');
-			binCalc.setCharAt(7, '1');
+    		codigoc.setCharAt(6, '1');
+			codigoc.setCharAt(7, '1');
 			if (mnemnonic[1].equals( "%S")| mnemnonic[1].equals( "%D")){
-				binCalc.setCharAt(5, '1');
+				codigoc.setCharAt(5, '1');
 			} else{
-				binCalc.setCharAt(3, '1');
+				codigoc.setCharAt(3, '1');
 			}
     	}
     	
     	if (mnemnonic[0].equals( "addw")){
-    		binCalc.setCharAt(6, '1');
+    		codigoc.setCharAt(6, '1');
     	}
     	
     	System.out.println(mnemnonic[0]);
-    	//System.out.println(mnemnonic[0].substring(0,1));
-    	System.out.println(binCalc);
     	
-    	return binCalc.toString();
+    	System.out.println(codigoc);
+    	
+    	return codigoc.toString();
     }
 
     /**
@@ -224,8 +251,8 @@ public class Code {
      * @return Opcode (String de 3 bits) com código em linguagem de máquina para a instrução.
      */
     public static String jump(String[] mnemnonic) {
-    	int length = mnemnonic.length;
-    	for (int i = 0; i < length ; i++){
+    	int size = mnemnonic.length;
+    	for (int i = 0; i < size ; i++){
     		   if (mnemnonic[i].equals( "jmp")){
     			   return "111";
     		   } else if(mnemnonic[i].equals( "je")) {
@@ -252,6 +279,7 @@ public class Code {
      * @return Valor em binário (String de 15 bits) representado com 0s e 1s.
      */
     public static String toBinary(String symbol) {
+    	// usando funcao pra transdormar para binario e colocando minimo de 15 cracteres para a string, com zero para substituir
     	return String.format("%15s", Integer.toBinaryString(Integer.parseInt(symbol))).replace(' ', '0');
     }
     
@@ -264,4 +292,3 @@ public class Code {
     	return null;
     }
 }
-
